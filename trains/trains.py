9b90a7e5cd99b44baaacd5e28def4cf9b3bc3a77
peternortonuk <peter.norton@gazprom-mt.com>
2019-01-12 16:12:10 +0000

    add disruption details

--- trains/trains.py
+++ trains/trains.py
@@ -6,22 +6,31 @@ api_key = open_ldbws_account_details['token']
 
 crs_london_waterloo = 'WAT'
 crs_godalming = 'GOD'
-report_properties = ['destination_text', 'destinations', 'estimated_arrival', 'estimated_departure', 'eta', 'etd', 'operator_code', 'operator_name', 'origin_text', 'origins', 'platform', 'scheduled_arrival', 'scheduled_departure', 'service_id', 'sta', 'std']
+service_properties = ['destination_text', 'destinations', 'estimated_arrival', 'estimated_departure', 'eta', 'etd', 'operator_code', 'operator_name', 'origin_text', 'origins', 'platform', 'scheduled_arrival', 'scheduled_departure', 'service_id', 'sta', 'std']
+service_detail_properties = ['disruption_reason', 'is_cancelled', 'overdue_message']
 
 
-darwin_sesh = DarwinLdbSession(wsdl=wsdl, api_key=api_key)
-board = darwin_sesh.get_station_board(crs=crs_godalming, destination_crs=crs_london_waterloo)
-
-services = board.train_services
-for service in services:
+def print_properties(item, properties):
     print('==============')
-    for property in report_properties:
+    for property in properties:
         try:
-            message = getattr(service, property)
+            message = getattr(item, property, None)
             print(property, ': ', message)
         except NotImplementedError:
             print(property, ': not implemented')
 
 
+session = DarwinLdbSession(wsdl=wsdl, api_key=api_key)
+board = session.get_station_board(crs=crs_godalming, destination_crs=crs_london_waterloo)
+services = board.train_services
+for service in services:
+    # print all the standard details
+    print_properties(service, service_properties)
 
+    # for each service print the extra disruption details
+    service_id = service.service_id
+    service_item = session.get_service_details(service_id)
+    print_properties(service_item, service_detail_properties)
+
+pass
 
