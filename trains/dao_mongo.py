8360e36f98fa7b546eed2a376d8dcdd50fb95f3c
pnorton <peter.norton@gazprom-mt.com>
2019-01-17 09:39:50 +0000

    insert into db working

--- trains/dao_mongo.py
+++ trains/dao_mongo.py
@@ -1,6 +1,11 @@
 from mongoengine import *
 connect(db='trains', host='localhost', port=27017)
 
+'''
+ensure mongodb server is running with
+C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe
+'''
+
 class Train(Document):
     service_id = StringField(required=True)
     service = DictField()
--- trains/trains.py
+++ trains/trains.py
@@ -1,6 +1,7 @@
 from nredarwin.webservice import DarwinLdbSession
-from credentials import open_ldbws_account_details
 from pprint import pprint as pp
+from credentials import open_ldbws_account_details
+from dao_mongo import Train
 
 wsdl = "https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx"
 api_key = open_ldbws_account_details['token']
@@ -10,36 +11,38 @@ crs_godalming = 'GOD'
 service_properties = ['destination_text', 'destinations', 'estimated_arrival', 'estimated_departure', 'eta', 'etd', 'operator_code', 'operator_name', 'origin_text', 'origins', 'platform', 'scheduled_arrival', 'scheduled_departure', 'service_id', 'sta', 'std']
 service_detail_properties = ['disruption_reason', 'is_cancelled', 'overdue_message']
 
-
-def print_properties(item, properties):
-    print('==============')
-    for property in properties:
-        try:
-            message = getattr(item, property, None)
-            print(property, ': ', message)
-        except NotImplementedError:
-            print(property, ': not implemented')
-
-
 session = DarwinLdbSession(wsdl=wsdl, api_key=api_key)
 board = session.get_station_board(crs=crs_godalming, destination_crs=crs_london_waterloo)
 services = board.train_services
 
-insert_dict = {}
 for service in services:
     # get the extra disruption details
     service_id = service.service_id
     service_item = session.get_service_details(service_id)
 
-    # prepare data to insert into db
+    # prepare data
     service_dict = vars(service)
     service_dict_key = service_dict.pop('_service_id')
     service_detail_dict = vars(service_item)
-    insert_dict[service_dict_key] = {'service': service_dict, 'details': service_detail_dict}
 
     # print details
     print('=============')
     pp(service_dict)
     pp(service_detail_dict)
+
+    # butchery to remove nredarwin objects
+    service_remove_items = ['_destinations', '_origins']
+    details_remove_items = ['_previous_calling_point_lists', '_subsequent_calling_point_lists']
+    for k in service_remove_items:
+        service_dict.pop(k)
+    for k in details_remove_items:
+        service_detail_dict.pop(k)
+
+    # insert into db
+    train = Train(service_id=service_id)
+    train.service = service_dict
+    train.details = service_detail_dict
+    train.save()
+
 pass
 
