d7d9789f95c8287e1f1f7a1e187d879620be9368
Peter Norton <peter.norton@gazprom-mt.com>
2019-12-23 21:42:39 +0000

    google api

---
+++ google-drive-sample/people/quickstart/quickstart.py
@@ -0,0 +1,67 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START people_quickstart]
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/contacts.readonly']
+
+def main():
+    """Shows basic usage of the People API.
+    Prints the name of the first 10 connections.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('people', 'v1', credentials=creds)
+
+    # Call the People API
+    print('List 10 connection names')
+    results = service.people().connections().list(
+        resourceName='people/me',
+        pageSize=10,
+        personFields='names,emailAddresses').execute()
+    connections = results.get('connections', [])
+
+    for person in connections:
+        names = person.get('names', [])
+        if names:
+            name = names[0].get('displayName')
+            print(name)
+
+if __name__ == '__main__':
+    main()
+# [END people_quickstart]
