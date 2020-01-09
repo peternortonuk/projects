7e6a9b9ce641694237f411257f6bd48bc07a335e
pnorton <peter.norton@gazprom-mt.com>
2020-01-09 15:43:47 +0000

    fix hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,11 @@
-import os
 import sys
 from pprint import pprint as pp
 
-print(os.getcwd())
 pp(sys.path)
-paths = ['C:\\dev\\code\\projects\\pytube\\pytube', 'C:\\dev\\code\\projects\\pytube\\pytube\\contrib']
+paths = ['C:\\dev\\code\\projects\\pytube']
 sys.path.extend(paths)
 pp(sys.path)
 
-os.chdir(r'C:\dev\code\projects\pytube')
 from pytube import YouTube
 
 url = r'https://www.youtube.com/watch?v=xZAfdgYnsZc&list=PLYf3-nc0C5ly-2-7WU6S2nCW7MMT0VM87&index=2'
