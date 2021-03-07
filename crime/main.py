70549f0c1520f1568258b510b938792f4fc228b5
Peter Norton <peter.norton@gazprom-mt.com>
2021-03-07 21:42:06 +0000

    crime wip

--- crime/main.py
+++ crime/main.py
@@ -11,13 +11,15 @@ https://maps.googleapis.com/maps/api/staticmap?center=63.259591,-144.667969&zoom
 """
 import pandas as pd
 from crime.get_data import read_giant_df_from_pickle
-from crime.constants import all_raw_pkl_file_name, FALLS_WITHIN, filter_falls_within, raw_url
+from crime.constants import all_raw_pkl_file_name, FALLS_WITHIN, filter_falls_within, raw_url, COLOR, CRIME_TYPE
+import seaborn as sns
 
 all_columns = ['Crime ID', 'Month', 'Reported by', 'Falls within', 'Longitude',
                'Latitude', 'Location', 'LSOA code', 'LSOA name', 'Crime type',
                'Last outcome category', 'Context']
 
 selected_columns = ['Month', 'Longitude', 'Latitude', 'Crime type']
+selected_columns = selected_columns + [COLOR]
 
 
 def get_parameters_from_df(df):
@@ -25,14 +27,46 @@ def get_parameters_from_df(df):
     return df[selected_columns]
 
 
-def build_full_url(url, parameters):
-    return url
+def add_color_for_column(df, col):
+    unique_vals = df[col].unique()
+    palette = sns.color_palette(None, len(unique_vals)).as_hex()
+    zz = zip(unique_vals, palette)
+    value_to_color_dict = dict(zz)
+    df[COLOR] = df.apply(lambda x: value_to_color_dict[x[col]], axis='columns')
+    return df
+
+
+def build_params_for_month(df, month):
+    mask = df['Month'] == month
+    df = df[mask]
+    df = df[:5]
+    dd = {}
+    for row in df.iterrows():
+        pass
+        # dd = markers=color:blue%7Clabel:S%7C62.107733
+    return dd
 
 
 if __name__ == '__main__':
+    # get all the data
     df = read_giant_df_from_pickle(all_raw_pkl_file_name)
+
+    # select the required geographical area
     mask = df[FALLS_WITHIN] == filter_falls_within
     df = df[mask]
-    parameters = get_parameters_from_df(df)
-    url = build_full_url(raw_url, parameters)
+
+    # add a colour based on unique values of a column
+    df = add_color_for_column(df, CRIME_TYPE)
+
+    # select columns required for url
+    parameters_df = get_parameters_from_df(df)
+
+    # exclude rows that have any nan values
+    mask = parameters_df.isnull().any(axis='columns')
+    parameters = parameters_df[~mask]
+
+    # build a set of url parameters
+    month = pd.to_datetime('2020-11-1')
+    dd = build_params_for_month(parameters_df, month)
+    pass
 
