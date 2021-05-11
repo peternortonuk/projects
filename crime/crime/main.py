13015624344089faf3dace416fb002da83d0d924
peter norton <peter.norton.uk@gmail.com>
2021-05-11 20:52:37 +0100

    added tests and type hints

--- crime/crime/main.py
+++ crime/crime/main.py
@@ -17,8 +17,8 @@ import os
 from collections import namedtuple
 
 from crime.crime.get_data import read_df_from_pickle, save_df_to_pickle
-from crime.crime.constants import all_raw_pkl_file_name, subset_raw_pkl_file_name, raw_url, crime_folder_name, html_file_name, \
-    MONTH, LONGITUDE, LATITUDE, CRIME_TYPE, FALLS_WITHIN, COLOR
+from crime.crime.constants import all_raw_pkl_file_name, subset_raw_pkl_file_name, raw_url, crime_folder_name, \
+    html_file_name, MONTH, LONGITUDE, LATITUDE, CRIME_TYPE, FALLS_WITHIN, COLOR
 from crime.crime.html_template import html_header, html_template_body, html_footer
 from crime.crime.credentials import api_key
 
@@ -62,26 +62,27 @@ data_selection = RunType.SUBSET
 # function definitions
 
 
-def get_date_from_string(df):
+def get_date_from_string(df: pd.DataFrame):
     df[MONTH] = pd.to_datetime(df[MONTH], format='%Y-%m')
 
 
-def create_colors_for_items(items):
+def create_colors_for_items(items: list) -> dict:
     palette = sns.color_palette(None, len(items)).as_hex()
-    palette = ['0x'+ss[1:] for ss in palette]  # replace # with 0x
+    palette = ['0x' + ss[1:] for ss in palette]  # replace # with 0x
     zz = zip(items, palette)
     return dict(zz)
 
 
-def build_string_for_location(row):
+def build_string_for_location(row: pd.Series) -> str:
     return f'{row.Latitude},{row.Longitude}'
 
 
-def build_string_for_marker(row):
+def build_string_for_marker(row: pd.Series) -> str:
     return f'markers=color:{row.Color}%7Csize:tiny'
 
 
-def apply_filters(df, filter_falls_within, filter_month, centre, delta):
+def apply_filters(df: pd.DataFrame, filter_falls_within: str, filter_month: datetime.date, centre: Location,
+                  delta: float) -> pd.DataFrame:
     # report period is expressed as string in the form yyyy-mm; convert to date
     get_date_from_string(df)
 
@@ -99,15 +100,15 @@ def apply_filters(df, filter_falls_within, filter_month, centre, delta):
     df = df[mask]
 
     # filter for area
-    condition1 = df[LATITUDE].between(centre.latitude-delta, centre.latitude+delta)
-    condition2 = df[LONGITUDE].between(centre.longitude-delta, centre.longitude+delta)
+    condition1 = df[LATITUDE].between(centre.latitude - delta, centre.latitude + delta)
+    condition2 = df[LONGITUDE].between(centre.longitude - delta, centre.longitude + delta)
     mask = condition1 & condition2
     df = df[mask]
 
     return df
 
 
-def build_df_marker(df):
+def build_df_marker(df: pd.DataFrame) -> pd.DataFrame:
     # get count of crimes per type and create aggregate df
     df_marker = df[CRIME_TYPE].value_counts().to_frame()
 
@@ -120,7 +121,7 @@ def build_df_marker(df):
     return df_marker
 
 
-def build_string_for_url(df, crime_type):
+def build_string_for_url(df: pd.DataFrame, crime_type: str) -> str:
     # build string for location
     mask = df[CRIME_TYPE] == crime_type
     string_for_locations = '%7C'.join(df.loc[mask, 'string'].values)
@@ -133,7 +134,7 @@ def build_string_for_url(df, crime_type):
     return string_for_marker + '%7C' + string_for_locations
 
 
-def build_url_parameters(centre, view, api_key):
+def build_url_parameters(centre: Location, view: View, api_key: str) -> tuple:
     # top and tail the url
     map_dict = {'center': f'{centre.latitude},{centre.longitude}',
                 'zoom': view.zoom,
@@ -148,7 +149,7 @@ def build_url_parameters(centre, view, api_key):
     return map_params, key_params
 
 
-def build_url_for_crime_type(df, crime_type, raw_url, map_params, key_params):
+def build_url_for_crime_type(df: pd.DataFrame, crime_type: str, raw_url: str, map_params: str, key_params: str) -> str:
     # build string for location
     big_string = build_string_for_url(df, crime_type)
 
@@ -156,7 +157,7 @@ def build_url_for_crime_type(df, crime_type, raw_url, map_params, key_params):
     return raw_url + '?' + map_params + '&' + big_string + '&' + key_params
 
 
-def build_html_for_crime_type(url, crime_type, filter_month ,html_template_body):
+def build_html_for_crime_type(url: str, crime_type: str, filter_month: datetime.date, html_template_body: str):
     # check its not too big
     # https://developers.google.com/maps/documentation/maps-static/start#url-size-restriction
     print(f'\n\n"{crime_type}"... has string length = {len(url)}')
@@ -170,18 +171,19 @@ def build_html_for_crime_type(url, crime_type, filter_month ,html_template_body)
         return None
 
 
-def build_html_file(df, df_marker, filter_month ,html_template_body, raw_url ,centre, view, api_key):
+def build_html_file(df: pd.DataFrame, df_marker: pd.DataFrame, filter_month: datetime.date,
+                    html_template_body: str, raw_url: str, centre: Location, view: View, api_key: str) -> str:
     map_params, key_params = build_url_parameters(centre, view, api_key)
     html = ''
     for crime_type in df_marker.index:
         url = build_url_for_crime_type(df, crime_type, raw_url, map_params, key_params)
-        html_statement = build_html_for_crime_type(url, crime_type, filter_month ,html_template_body)
+        html_statement = build_html_for_crime_type(url, crime_type, filter_month, html_template_body)
         html = html + html_statement
     html_file = html_header + html + html_footer
     return html_file
 
 
-def write_to_file(html_file, pathfile):
+def write_to_file(html_file: str, pathfile: str):
     # create the file, write to it and close it
     f = open(pathfile, 'w')
     f.write(html_file)
@@ -241,4 +243,3 @@ if __name__ == '__main__':
         webbrowser.open_new_tab(pathfile)
     else:
         raise NotImplementedError
-
--- crime/tests/unit/test_functions.py
+++ crime/tests/unit/test_functions.py
@@ -5,7 +5,7 @@ C:\dev\code\projects (master -> origin)
 
 import unittest
 import pandas as pd
-from crime.crime.main import get_date_from_string
+from crime.crime.main import get_date_from_string, create_colors_for_items, build_string_for_location
 
 
 class TestFunctions(unittest.TestCase):
@@ -17,4 +17,28 @@ class TestFunctions(unittest.TestCase):
         truth = pd.DataFrame(data=[pd.to_datetime('2020-11-01')], columns=columns)
 
         pd.testing.assert_frame_equal(df, truth)
+
+    def test_create_colors_for_items(self):
+        lst = list('abcd')
+        result = create_colors_for_items(lst)
+
+        truth = {'a': '0x1f77b4', 'b': '0xff7f0e', 'c': '0x2ca02c', 'd': '0xd62728'}
+
+        assert truth == result
+
+    def test_build_string_for_location(self):
+        data = [{'Latitude': 54.123, 'Longitude': 0.123, },
+                {'Latitude': 53.000, 'Longitude': 0.000, }]
+        df = pd.DataFrame(data=data)
+
+        result = []
+        for _, row in df.iterrows():
+            ss = build_string_for_location(row)
+            result.append(ss)
+
+        truth = ['54.123,0.123', '53.0,0.0']
+
+        assert truth == result
+
+
 
