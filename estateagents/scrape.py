c9b19cbc6f3359ea765bfbfde485c20bee861ea5
peter norton <peter.norton.uk@gmail.com>
2021-07-07 19:26:02 +0100

    fix floorplan

--- estateagents/scrape.py
+++ estateagents/scrape.py
@@ -2,14 +2,20 @@ import urllib.request
 import re
 from bs4 import BeautifulSoup
 
-page_url = r'https://www.rightmove.co.uk/properties/104647769#/'
+ids = [104647769]
+id = ids[0]
+
+page_url_template = r'https://www.rightmove.co.uk/properties/{id}#/'
+floorplan_url_endpoint = r'floorplan?activePlan=1'
+
+# find photos
+page_url = page_url_template.format(id=id)
 
 with urllib.request.urlopen(page_url) as response:
     html_doc = response.read()
 
 soup = BeautifulSoup(html_doc, 'html.parser')
 
-# find photos
 photo_urls = []
 tags = soup.head.find_all('meta')
 for tag in tags:
@@ -17,12 +23,20 @@ for tag in tags:
         photo_urls.append(tag['content'])
 
 # find floorplan
-tags = soup.body
-tags = tags.find_all(href=re.compile('floorplan'))
-floorplan_url = tags[0].attrs.get('href')
-floorplan_url = floorplan_url[2:]  # hackhackhack
-floorplan_url = page_url + floorplan_url
-pass
+floorplan_url = page_url + floorplan_url_endpoint
+
+with urllib.request.urlopen(floorplan_url) as response:
+    html_doc = response.read()
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+[tag] = soup.body.find_all(alt='Floorplan')
+floorplan_image_url = tag['src']
+
+# remove the image size control _max_296x197.jpeg
+pattern = r'_max_\d+x\d+'
+replace = ''
+floorplan_image_url = re.sub(pattern, replace, floorplan_image_url)
 
 
 pass
