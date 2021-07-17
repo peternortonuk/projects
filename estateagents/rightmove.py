9a71eca7b6a4275cad5ee5d5ee09178a33cbb72b
peter norton <peter.norton.uk@gmail.com>
2021-07-17 13:44:49 +0100

    wip

--- estateagents/rightmove.py
+++ estateagents/rightmove.py
@@ -1,12 +1,24 @@
 import urllib.request
 import re
 from bs4 import BeautifulSoup
+import os
 
 ids = [104647769]
 id = ids[0]
+properties_dict = {id:
+    {
+        'photo_urls': [],
+        'floorplan_url': None,
+        'photo_filenames': [],
+        'floorplan_image_filename': None,
+        'property_text': None,
+    }
+}
 
 page_url_template = r'https://www.rightmove.co.uk/properties/{id}#/'
 floorplan_url_endpoint = r'floorplan?activePlan=1'
+project_folder = r'c:\dev\code\projects\estateagents'
+image_folder = os.path.join(project_folder, 'images')
 
 # find photos
 page_url = page_url_template.format(id=id)
@@ -16,16 +28,15 @@ with urllib.request.urlopen(page_url) as response:
 
 soup = BeautifulSoup(html_doc, 'html.parser')
 
-photo_urls = []
 tags = soup.head.find_all('meta')
 for tag in tags:
     if tag.attrs.get('property') == 'og:image':
-        photo_urls.append(tag['content'])
+        properties_dict[id]['photo_urls'].append(tag['content'])
 
 # find floorplan
-floorplan_url = page_url + floorplan_url_endpoint
+properties_dict[id]['floorplan_url'] = page_url + floorplan_url_endpoint
 
-with urllib.request.urlopen(floorplan_url) as response:
+with urllib.request.urlopen(properties_dict[id]['floorplan_url']) as response:
     html_doc = response.read()
 
 soup = BeautifulSoup(html_doc, 'html.parser')
@@ -36,9 +47,11 @@ floorplan_image_url = tag['src']
 # remove the image size control _max_296x197.jpeg
 pattern = r'_max_\d+x\d+'
 replace = ''
-floorplan_image_url = re.sub(pattern, replace, floorplan_image_url)
+properties_dict[id]['floorplan_url'] = re.sub(pattern, replace, properties_dict[id]['floorplan_url'])
 
 # get text description and price and things
+this_property_image_folder = os.path.join(image_folder, str(id))
+urllib.urlretrieve(floorplan_image_url, this_property_image_folder)
 
 # get photos
 
